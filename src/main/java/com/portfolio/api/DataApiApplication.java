d8acf360eb7c3d06cce248e65f357132d635787c
dnjsgml511 <dnjsgml511@naver.com>
2021-07-04 20:29:04 +0900

    jwt 테스트

---
+++ src/main/java/com/portfolio/api/DataApiApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/portfolio/api/login/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.portfolio.api.login;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends CrudRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
